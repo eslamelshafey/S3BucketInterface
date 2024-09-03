c70b532d0772db0e4ac71932d599144d9587357d
eslamelshafey <eslamshafey5056@gmail.com>
2024-09-03 23:23:43 +0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,15 @@
+  <?php
+
+return [
+    'pass'=>'1234',
+    's3'=>[
+        'bucket'=>'ftmsa',
+        'credentials'=>[
+            'key'=>'AKIA5DUW3IPYL42M3LV3',
+            'secret'=>'',
+        ],
+        'version'=>'latest',
+        'region'=>'me-south-1',
+    ],
+    'BASE_URL'=>"https://localhost/uploads/upload/",
+];
